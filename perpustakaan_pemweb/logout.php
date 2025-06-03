780373a3a318843a572fc6b085e99fc38e04286e
mekka <143987030+zskiamekaa@users.noreply.github.com>
2025-06-03 15:32:02 +0700

    Delete perpustakaan_pemweb directory

--- perpustakaan_pemweb/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();    // Hapus semua variabel session
-session_destroy();  // Hancurkan session
-header("Location: login.php");  // Redirect ke halaman login
-exit();
-?>
